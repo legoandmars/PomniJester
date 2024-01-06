6b057165d92070a0d9c4116140e3005a769cb8ee
legoandmars <legoandmars@gmail.com>
2024-01-05 18:29:04 -0800

    initial commit

---
+++ PomniJester/Patches/JesterPatch.cs
@@ -0,0 +1,63 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace PomniJester.Patches
+{
+    [HarmonyPatch(typeof(JesterAI))]
+    internal class JesterPatch
+    {
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        public static void JesterStart(JesterAI __instance)
+        {
+
+            if (Plugin.JesterTexture == null || Plugin.JesterClip == null || Plugin.JesterHat == null || Plugin.JesterPopClip == null)
+            {
+                Plugin.Logger.LogWarning("Pomni Jester replacement assets null, returning");
+                return;
+            }
+
+            var hatParent = __instance.transform.Find("MeshContainer/AnimContainer/metarig/BoxContainer/spine.004/spine.005/spine.006/UpperJaw");
+            if (hatParent == null)
+            {
+                Plugin.Logger.LogWarning("Could not find jester hat parent, returning");
+                return;
+            }
+
+            Material? matReference = null;
+            // swap skin
+            // matReferences is needed because HDRP/Lit shaders exported through assetbundles aren't *quite* right
+            // Other shaders (such as custom & unlit shaders) SOMETIMES work, but we'll need an HDRP/Lit reference to use the shader in this case
+            var materials = __instance.GetComponentsInChildren<Renderer>(true).SelectMany(x => x.sharedMaterials).ToList();
+            foreach (var material in materials)
+            {
+                if (material.name.StartsWith("JesterTex"))
+                {
+                    // replace
+                    material.mainTexture = Plugin.JesterTexture;
+                    matReference = material;
+                }
+            }
+
+            // swap audio
+            __instance.popGoesTheWeaselTheme = Plugin.JesterClip;
+            __instance.popUpSFX = Plugin.JesterPopClip;
+
+            var hat = UnityEngine.Object.Instantiate(Plugin.JesterHat);
+            hat.transform.SetParent(hatParent);
+            hat.transform.localPosition = Vector3.zero;
+            hat.transform.localRotation = Quaternion.identity;
+            hat.transform.localScale = Vector3.one;
+
+            if (matReference == null) return;
+            foreach (var renderer in hat.GetComponentsInChildren<Renderer>(true))
+            {
+                renderer.sharedMaterial.shader = matReference.shader;
+            }
+        }
+    }
+}
---
+++ PomniJester/Plugin.cs
@@ -0,0 +1,43 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace PomniJester
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static new ManualLogSource Logger;
+
+        public static Texture JesterTexture;
+        public static GameObject JesterHat; // needs shader swapped out at runtime
+        public static AudioClip JesterClip;
+        public static AudioClip JesterPopClip;
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger = base.Logger;
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+            LoadAssets();
+        }
+
+        private void LoadAssets()
+        {
+            // find file named "jestervisuals" in the same folder as the plugin
+            var bundlePath = Path.Join(Path.GetDirectoryName(this.Info.Location), "jestervisuals");
+            var bundle = AssetBundle.LoadFromFile(bundlePath);
+            var asset = bundle.LoadAsset<GameObject>("assets/prefabs/jestervisuals.prefab");
+
+            // little hardcoded, didn't wanna set up a class
+            JesterTexture = asset.transform.GetChild(0).GetComponent<Renderer>().sharedMaterial.mainTexture;
+            JesterHat = asset.transform.GetChild(1).gameObject;
+            JesterClip = asset.transform.GetChild(2).GetComponent<AudioSource>().clip;
+            JesterPopClip = asset.transform.GetChild(3).GetComponent<AudioSource>().clip;
+        }
+    }
+}
